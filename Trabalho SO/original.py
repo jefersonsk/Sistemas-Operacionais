d335210700db0fd5992172490c130dfe4245f084
Jeferson da Silveira Kendzierski <jefo.silveira@gmail.com>
2026-09-14 10:23:53 -0300

    Retirado print
    Retirada a linha do print que está passando informações de tempo restante do arquivo original.

--- Trabalho SO/original.py
+++ Trabalho SO/original.py
@@ -132,7 +132,6 @@ def FCFS(execucao, espera, restante, chegada):
 
         if tempo_execucao[processo_em_execucao] == tempo_restante[processo_em_execucao]:
             tempo_espera[processo_em_execucao] = i - 1
-            print(tempo_espera)
         if tempo_restante[processo_em_execucao] == 1:
             if processo_em_execucao == (n_processos - 1):
                 break
